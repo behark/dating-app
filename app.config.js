3d98f64dd8715966b1517d5a6c59855eb776544b
behark <beharkabashi22@gmail.com>
2026-03-21 19:59:20 +0100

    chore(version): bump version code to 44

--- app.config.js
+++ app.config.js
@@ -69,7 +69,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.beharkabashis.datingapp',
-      buildNumber: '43',
+      buildNumber: '44',
       // Deep linking support
       associatedDomains: ['applinks:dating-app.com', 'applinks:*.dating-app.com'],
       infoPlist: {
@@ -111,7 +111,7 @@ export default {
     // Android Configuration - Minimum Android 8.0 (API 26)
     android: {
       package: 'com.beharkabashis.datingapp',
-      versionCode: 43,
+      versionCode: 44,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
         backgroundColor: '#ffffff',
